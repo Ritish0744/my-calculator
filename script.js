cc30b84102bcf3681ca19e44cfb97d935485c15b
Ritish0744 <ritishverms@gmail.com>
2025-07-18 21:18:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
- submit.onclick = function () {
-        let num1 = Number(document.getElementById(`input1`).value);
-        let num2 = Number(document.getElementById(`input2`).value);
-        let submit = document.getElementById(`submit`);
-        let dropdown = document.getElementById(`dropdown`).value;
-        let result = document.getElementById(`result`);
+let display = document.getElementById(`display`);
+let result;
+function num(no) {
+  display.value = display.value + no;
+}
 
-        if (dropdown === `plus`) {
-          result.textContent = `Result is ${num1 + num2}`;
-        } else if (dropdown === `minus`) {
-          result.textContent = `Result is ${num1 - num2}`;
-        } else if (dropdown === `divide`) {
-          result.textContent = `Result is ${num1 / num2}`;
-        } else if (dropdown === `multiply`) {
-          result.textContent = `Result is ${num1 * num2}`;
-        } else {
-          result.textContent = `Select an Operator`;
-        }
-      }
+function op(op) {
+  display.value = display.value + op;
+}
+
+function back() {
+  display.value = display.value.slice(0, -1);
+}
+function equal() {
+  display.value = eval(display.value);
+}
+
+function clr(){
+  display.value = ``;
+}
